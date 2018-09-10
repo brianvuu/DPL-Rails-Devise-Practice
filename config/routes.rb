79ae6fc6b31689a8699921306ccb463a81b3f498
Brian Vuu <brianvuu@gmail.com>
2018-09-09 20:14:09 -0600

    setup model, controller, and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'accounts/index'
+  get 'accounts/show'
+  get 'accounts/new'
+  get 'accounts/edit'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
