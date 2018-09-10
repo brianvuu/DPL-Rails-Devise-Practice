6dbc8df0365b22f08f9fb13b51f760402d581d44
Brian Vuu <brianvuu@gmail.com>
2018-09-09 20:22:05 -0600

    congfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'accounts/index'
-  get 'accounts/show'
-  get 'accounts/new'
-  get 'accounts/edit'
+  root to: 'accounts#index'
+  # get 'accounts/show'
+  # get 'accounts/new'
+  # get 'accounts/edit'
   devise_for :users
+  resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
